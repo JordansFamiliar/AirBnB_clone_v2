4994d4ccecd0f3e846a8ae4466954d44a80d2ad8
98KateMo <kr.molefe98@gmail.com>
2023-10-25 09:11:41 -0700

    files

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This module defines the Amenity class for the HBNB project."""
 
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class for storing amenity information."""
+
+    # Define the table name in the database
+    __tablename__ = 'amenities'
+
+    # Check the storage type to determine how to handle 'name'
+    if storage_type == 'db':
+        # Define 'name' as a column in the database
+        name = Column(String(128), nullable=False)
+    else:
+        # If not using a database, set 'name' as an empty string
+        name = ""
--- models/review.py
+++ models/review.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""This module defines the Review class for the HBNB project."""
 
+from sqlalchemy.sql.schema import ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from models import storage_type
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """Review class for storing review information."""
+
+    # Define the table name in the database
+    __tablename__ = 'reviews'
+
+    # Check the storage type to determine how to handle attributes
+    if storage_type == 'db':
+        # Define 'text' as a column in the database
+        text = Column(String(1024), nullable=False)
+        # Define 'place_id' as a foreign key column linked to 'places.id'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        # Define 'user_id' as a foreign key column linked to 'users.id'
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        # If not using a database, set 'place_id' and 'user_id' as empty strings
+        place_id = ""
+        user_id = ""
+        text = ""
